aaac4538a7c7d592e1098355266f8bd2854068cd
Ole P <olepor@matnat.uio.no>
2015-10-31 11:23:32 +0100

    added gard's directory sort, and mathias' dev-mode as submodules

--- init.el
+++ init.el
@@ -15,17 +15,7 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/devilry-mode/")
 (require 'devilry-mode)
 
-(defun tangle-init ()
-  "If the current buffer is 'init.org' the code-blocks are
-    tangled, and the tangled file is compiled."
-  (when (equal (buffer-file-name)
-               (expand-file-name (concat user-emacs-directory "init.org")))
-    ;; Avoid running hooks when tangling.
-    (let ((prog-mode-hook nil))
-      (org-babel-tangle)
-      (byte-compile-file (concat user-emacs-directory "init.el")))))
 
-(add-hook 'after-save-hook 'tangle-init)
 
 ;; Packages
    
