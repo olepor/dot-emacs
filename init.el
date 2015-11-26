fd599abca4f71b14e46932a279acae1dad3126d7
Ole P <olepor@matnat.uio.no>
2015-11-26 21:29:19 +0100

    stole some gems off larstvei

--- init.el
+++ init.el
@@ -1,10 +1,51 @@
+(require 'cl)
+(require 'package)
+(package-initialize)
 ;; packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
-(package-initialize)
-
+(let* ((packages
+        '(auto-compile         ; automatically compile Emacs Lisp libraries
+          cider                ; Clojure Interactive Development Environment
+          company              ; Modular text completion framework
+          define-word          ; display the definition of word at point
+          diminish             ; Diminished modes from modeline
+          expand-region        ; Increase selected region by semantic units
+          focus                ; Dim color of text in surrounding sections
+          idle-require         ; load elisp libraries while Emacs is idle
+          geiser               ; GNU Emacs and Scheme talk to each other
+          git-gutter-fringe    ; Fringe version of git-gutter.el
+          haskell-mode         ; A Haskell editing mode
+          helm                 ; Incremental and narrowing framework
+          helm-company         ; Helm interface for company-mode
+          helm-projectile      ; Helm integration for Projectile
+          helm-swoop           ; Efficiently hopping squeezed lines
+          jedi                 ; Python auto-completion for Emacs
+          js2-mode             ; Improved JavaScript editing mode
+          magit                ; control Git from Emacs
+          markdown-mode        ; Emacs Major mode for Markdown-formatted files
+          matlab-mode          ; MATLAB integration with Emacs
+          maude-mode           ; Emacs mode for the programming language Maude
+          monokai-theme        ; A fruity color theme for Emacs
+          move-text            ; Move current line or region with M-up or M-down
+          multiple-cursors     ; Multiple cursors for Emacs.
+          olivetti             ; Minor mode for a nice writing environment
+          org                  ; Outline-based notes management and organizer
+          paredit              ; minor mode for editing parentheses
+          pdf-tools            ; Emacs support library for PDF files.
+          projectile           ; Manage and navigate projects in Emacs easily
+          slime                ; Superior Lisp Interaction Mode for Emacs
+          try))                ; Try out Emacs packages
+       ;; Remove all packages already installed
+       (packages (remove-if 'package-installed-p packages)))
+  (when packages
+    (ignore-errors (package-refresh-contents)
+                   (mapcar 'package-install packages)
+                   ;; This package is only relevant for Mac OS X.
+                   (when (memq window-system '(mac ns))
+                     (package-install 'exec-path-from-shell)))))
 (defun require-package (package)
   (setq-default highlight-tabs t)
   "Install given PACKAGE."
@@ -12,6 +53,41 @@
     (unless (assoc package package-archive-contents)
       (package-refresh-contents))
     (package-install package)))
+
+
+(setq auto-revert-interval 1        ; Refresh buffers fast
+      default-input-method "TeX"    ; Use TeX when toggling input method.
+      doc-view-continuous t         ; At page edge goto next/previous.
+      echo-keystrokes 0.1           ; Show keystrokes asap.
+      inhibit-startup-message t     ; No splash screen please.
+      initial-scratch-message nil   ; Clean scratch buffer.
+      ring-bell-function 'ignore    ; Quiet.
+sentence-end-double-space nil); No double space
+;; Some mac-bindings interfere with Emacs bindings.
+(when (boundp 'mac-pass-command-to-system)
+  (setq mac-pass-command-to-system nil))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Set encoding to always be UTF-8
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+
+
+
+;; Highlight comment annotations
+(defun lokaltog-font-lock-comment-annotations ()
+  "Highlight a bunch of well known comment annotations.
+This functions should be added to the hooks of major modes for programming."
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):?"
+          1 font-lock-warning-face t))))
+
+(add-hook 'prog-mode-hook 'lokaltog-font-lock-comment-annotations)
+
+
 
 (package-initialize)
 (evil-mode 1)
